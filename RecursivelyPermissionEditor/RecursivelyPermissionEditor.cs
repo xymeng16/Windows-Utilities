ba30c7ee833ed2318b54703c5a7ab20820c90e5d
Xiangyi Meng <Xiangyi Meng>
2017-04-29 18:02:22 +0800

    Add sqlconnector and recursivelypermissioneditor.

---
+++ RecursivelyPermissionEditor/RecursivelyPermissionEditor.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.AccessControl;
+namespace RecursivelyPermissionEditor
+{
+    enum Mode
+    {
+        ADD, REMOVE
+    };
+
+    enum Type
+    {
+        FILE, DIRECTORY
+    };
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 1)
+            {
+                Console.Out.WriteLine("Usage: RecurPermissionEditor [dir1] [dir2] ...");
+            }
+            for (int i = 0; i < args.Length; i++)
+            {
+                DFSDirTraverse(args[i]);
+            }
+            
+        }
+        public static void DFSDirTraverse(string dir)
+        {
+            try
+            {
+                foreach (var d in Directory.GetDirectories(dir))
+                {
+                    ModifySecurity(Type.DIRECTORY, d, "Everyone", FileSystemRights.FullControl, AccessControlType.Allow, Mode.ADD);
+                    foreach (var f in Directory.GetFiles(d))
+                    {
+                        ModifySecurity(Type.FILE, f, "Everyone", FileSystemRights.FullControl, AccessControlType.Allow, Mode.ADD);
+                    }
+                    DFSDirTraverse(d);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Removes an ACL entry on the specified file for the specified account.
+        /// </summary>
+        /// <param name="fileName">The name of the file needed to be modified.</param>
+        /// <param name="account">This parameter delcares which user that the rights belong to.</param>
+        /// <param name="rights">Thr rights to be added or removed.</param>
+        /// <param name="controlType">The type of the access control.</param>
+        
+        public static void ModifySecurity(Type type, string fileName, string account,
+            FileSystemRights rights, AccessControlType controlType, Mode mode)
+        {
+            FileSystemSecurity Security;
+
+            // Get a FileSecurity object that represents the
+            // current security settings.
+            if (Type.FILE == type)
+                Security = File.GetAccessControl(fileName);
+            else
+                Security = Directory.GetAccessControl(fileName);
+
+            if (Mode.ADD == mode)
+                Security.AddAccessRule(new FileSystemAccessRule(account, rights, controlType));
+            // Remove the FileSystemAccessRule from the security settings.
+            else
+                Security.RemoveAccessRule(new FileSystemAccessRule(account, rights, controlType));
+
+            // Set the new access settings.
+            if (Type.FILE == type)
+                File.SetAccessControl(fileName, Security as FileSecurity);
+            else
+                Directory.SetAccessControl(fileName, Security as DirectorySecurity);
+
+        }
+    }
+}
+
---
+++ SQLConnector/SqlConnector.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SQLServerConnection
+{
+    public class SqlConnector
+    {
+        private String _connectionString;
+        private SqlConnection _sqlConn;
+        public SqlConnector(String connectionStr)
+        {
+            _connectionString = connectionStr;
+            _sqlConn = new SqlConnection(connectionStr);
+        }
+        public void Connect()
+        {
+            try
+            {
+                _sqlConn.Open();
+            }
+            catch (SqlException e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public void Disconnect()
+        {
+            try
+            {
+                _sqlConn.Close();
+            }
+            catch (SqlException e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public DataSet Query(String querySqlStr, string tableName)
+        {
+            SqlDataAdapter sqlDatAdptr = new SqlDataAdapter(querySqlStr, _sqlConn);
+
+            DataSet result = new DataSet();
+
+            try
+            {
+                sqlDatAdptr.Fill(result, tableName);
+                return result;
+            }
+            catch (SqlException e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                sqlDatAdptr.Dispose();
+            }
+        }
+
+        public int QueryWithoutResult(String querySqlStr, String tableName)
+        {
+            SqlCommand sqlCmd = new SqlCommand(querySqlStr, _sqlConn);
+            try
+            {
+                int rows = sqlCmd.ExecuteNonQuery();
+                return rows;
+            }
+            catch (SqlException e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                sqlCmd.Dispose();
+            }
+        }
+    }
+
+    
+}
+
